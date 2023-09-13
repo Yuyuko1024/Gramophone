2ce4a0607b57278d9c0890ea1491ada7d105c414
nift4 <nift4@protonmail.com>
2023-09-13 18:25:39 +0200

    Revert "BasePreferences: I perfer Z axis transitions :P"
    This reverts commit 7e35d1196dc1488a35bbb570ea1e6a14e00146d4.

--- app/src/main/java/org/akanework/gramophone/ui/fragments/BaseFragment.kt
+++ app/src/main/java/org/akanework/gramophone/ui/fragments/BaseFragment.kt
@@ -2,6 +2,8 @@ package org.akanework.gramophone.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.transition.MaterialSharedAxis
@@ -12,10 +14,10 @@ abstract class BaseFragment(val wantsPlayer: Boolean) : Fragment() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, /* forward= */ true)
-		returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, /* forward= */ false)
-		exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, /* forward= */ true)
-		reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, /* forward= */ false)
+		enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+		returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+		exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+		reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
 	}
 
 	// https://github.com/material-components/material-components-android/issues/1984#issuecomment-1089710991
